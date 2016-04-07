e4da675e62a24c0a936b30bbccd59a8a1a11faa1
jllçj <rails new hlhj>
2016-04-07 17:26:52 -0300

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
     if resource.sign_in_count == 1
-         principal_index_path 
+         principal_path 
     elsif resource.admin?
         admin_index_path
     else
