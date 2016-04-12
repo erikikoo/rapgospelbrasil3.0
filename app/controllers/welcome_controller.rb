77aba16c27410c244eb6dd94d6cccd605789fee1
Erik R de Souza <erikikoo@hotmail.com>
2016-04-12 16:02:08 -0300

    'atualizacao'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,7 @@ class WelcomeController < ApplicationController
   end
 
   def noticia
-  	@notices = Notice.order(created_at: :desc)
-    
+  	@notices = Notice.order(created_at: :desc)    
   	render 'notices/index'
   end
 
@@ -14,7 +13,7 @@ class WelcomeController < ApplicationController
   end
 
   def artista
-    @artist_data = ArtistData.where("'aprovado' = ? AND 'bloqueado' = ? AND nome IS NOT ?", 0, false, nil)
+    @artist_data = ArtistData.where("'aprovado' = ? AND 'bloqueado' = ? AND nome IS NOT ?", true, false, nil)
     render 'artist_datas/index'
   end
 
