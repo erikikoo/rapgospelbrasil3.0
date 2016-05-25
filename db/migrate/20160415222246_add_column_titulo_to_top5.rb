acf847c3d95440148d40fd8e7accf5ca68dfa2d4
Erik R. de Souza <erikikoo@hotmail.com>
2016-05-25 18:28:24 -0300

    'atualizacao'

--- db/migrate/20160415222246_add_column_titulo_to_top5.rb
+++ db/migrate/20160415222246_add_column_titulo_to_top5.rb
@@ -1,12 +1,12 @@
 class AddColumnTituloToTop5 < ActiveRecord::Migration
   def change
     add_column :top5s, :titulo2, :string
-    add_column :top5s, :rank2, :integer
+    add_column :top5s, :rank2, :string
     add_column :top5s, :titulo3, :string
-    add_column :top5s, :rank3, :integer
+    add_column :top5s, :rank3, :string
     add_column :top5s, :titulo4, :string
-    add_column :top5s, :rank4, :integer
+    add_column :top5s, :rank4, :string
     add_column :top5s, :titulo5, :string
-    add_column :top5s, :rank5, :integer
+    add_column :top5s, :rank5, :string
   end
 end
