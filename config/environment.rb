53b58e387498e0bac6bfd10644523b155547736f
Erik R. de Souza <erikikoo@hotmail.com>
2016-06-09 15:55:58 -0300

    'atualização'

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'erikikoo@hotmail.com',
+  :password => 'Pa081215',
+  :domain => 'young-reaches-99974.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # SMTP settings for mailgun
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.default_url_options = { :host => 'young-reaches-99974.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.hotmail.com' }
 # ActionMailer Config
 # Setup for production - deliveries, no errors raised
 config.action_mailer.delivery_method = :smtp
