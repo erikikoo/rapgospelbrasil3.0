171929b4292af50ab613f47fbb0869bf9c99a8ef
Erik R. de Souza <erikikoo@hotmail.com>
2016-08-08 16:16:08 -0300

    'UpdatePagSendNotice'

---
+++ app/controllers/eventos_controller.rb
@@ -0,0 +1,27 @@
+class EventosController < ApplicationController
+
+	def new
+		@event = Evento.new
+		respond_to do |format|
+			format.js {render '/welcome/event'}
+		end
+	end
+
+	def create
+		@event = Evento.new(params[:evento])
+		@event.request = request
+		if @event.deliver
+			@status = 'success'
+			@action = 'create'
+			render 'welcome/pags_index/checkout'
+		else
+			@status = 'danger'
+			@action = 'create'
+			respond_to do |format|
+				format.js {render '/welcome/pags_index/event'}
+			end
+		end
+	end
+end
+
+
---
+++ app/mailers/event.rb
@@ -0,0 +1,2 @@
+class Event < ApplicationMailer
+end
