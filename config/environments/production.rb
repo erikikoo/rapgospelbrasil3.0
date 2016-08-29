6747eef12dcfaf06cb3b6b6b46ddaa0bc5801222
Erik R. de Souza <erikikoo@hotmail.com>
2016-08-29 13:14:31 -0300

    'Update'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,20 +68,6 @@ Rails.application.configure do
   # SMTP settings for mailgun
 #   config.action_mailer.delivery_method = :smtp
   
-config.action_mailer.default_url_options = { :host => 'https://young-reaches-99974.herokuapp.com' }
-# # ActionMailer Config
-# # Setup for production - deliveries, no errors raised
- config.action_mailer.delivery_method = :smtp
-# config.action_mailer.perform_deliveries = true
-
- config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,  
-  user_name: '240382013@eniac.edu.br',
-  password: 'HayHelena',
-  authentication: "plain",
-  enable_starttls_auto: true
- }
 
 # config.action_mailer.raise_delivery_errors = false
 # config.action_mailer.default :charset => "utf-8"
